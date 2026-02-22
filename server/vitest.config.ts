8f5d93da68f9dbac3662ec112cd0a4ae3898870f
Duong Nam <dnam281224@gmail.com>
2026-02-22 19:54:00 +0700

    modify test coverage

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -9,12 +9,11 @@ export default defineConfig({
         coverage: {
             provider: "v8",
             reporter: ["text", "json", "html"],
-            include: ["src/**/*.ts"],
-            exclude: ["src/**/*.test.ts", "src/config/db.ts", "src/app.ts"],
+            include: ["src/controllers/*.ts", "src/services/*.ts"],
             thresholds: {
                 lines: 80,
                 functions: 80,
-                branches: 70,
+                branches: 80,
                 statements: 80
             }
         },
